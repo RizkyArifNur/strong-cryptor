a55cbf369400fc7452b901c52a080f100f512456
RizkyArifNur <rizkyarifnur@gmail.com>
2019-07-31 16:52:18 +0700

    feat(encryption): change encryption process
    change encryption process that will not need the separator
    
    BREAKING CHANGE: remove the separator

--- src/decryptor.ts
+++ src/decryptor.ts
@@ -1,7 +1,7 @@
 import crypto from 'crypto'
 import { ALGORITHM } from './config'
-import { InvalidKeyError, InvaliSeparatorError, MalformatedError } from './provider/'
-import { base64Regex, base64RegexValidSeparator, hexRegex } from './regex'
+import { InvalidKeyError, MalformatedError } from './provider/'
+import { base64Regex, hexRegex } from './regex'
 import { Encoding } from './typings'
 import { getIvAndEncryptedDataOnly } from './utils'
 /**
@@ -11,19 +11,13 @@ import { getIvAndEncryptedDataOnly } from './utils'
  * @param encoding string/text encoding you can choose `base64`(default) or `hex` (must be same with the encryption process)
  * @param separator `(optional)` this value used to separate the iv and the encrypted data (must be same with the encryption process), `default = :` (colon) don't using a valid `hex` and `base64` value for the separator!!
  */
-export function decrypt(encryptedData: string, key: string, encoding: Encoding = 'base64', separator: string = ':') {
+export function decrypt(encryptedData: string, key: string, encoding: Encoding = 'base64') {
   if (key.length !== 32) {
     throw new InvalidKeyError('Key must be 32 characters')
   }
-  if (separator.length !== 1) {
-    throw new InvaliSeparatorError('Separator must be one character')
-  }
-  if (encoding === 'hex' && hexRegex.test(separator)) {
-    throw new InvaliSeparatorError('Please input separator that exclude the hexadecimal value')
-  } else if (encoding === 'base64' && base64RegexValidSeparator.test(separator)) {
-    throw new InvaliSeparatorError('Please input separator that exclude the base64 value')
-  }
-  const { ivString, encryptedDataString } = getIvAndEncryptedDataOnly(encryptedData, separator)
+
+  const { ivString, encryptedDataString } = getIvAndEncryptedDataOnly(encryptedData, encoding)
+
   if (encoding === 'base64' && !base64Regex.test(ivString)) {
     throw new MalformatedError('Encrypted data malformated')
   } else if (encoding === 'hex' && !hexRegex.test(ivString)) {
