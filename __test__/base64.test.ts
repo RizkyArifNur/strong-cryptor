2bf2f8d593c332eba72a5d2e5596b31529cea71d
RizkyArifNur <rizkyarifnur@gmail.com>
2019-10-26 07:10:22 +0700

    test(encryption test): update test schema

--- __test__/base64.test.ts
+++ __test__/base64.test.ts
@@ -1,48 +1,53 @@
-import { decrypt, encrypt } from '../src'
+// tslint:disable: deprecation
+import { readFileSync, unlinkSync } from 'fs'
+import { join } from 'path'
+import { Decryptor, Encryptor } from '../src'
 import { ErrorProvider } from '../src/provider'
 import { base64Regex } from '../src/regex'
 import { getIvAndEncryptedDataOnly } from '../src/utils'
 const key = 'agsbsfsrstsgsfsrsfsrsfsrsgsnshwe'
 const text = 'Hello Fulan!'
 const encryptionCount = 12
+const encryptor = new Encryptor({ key })
+const decryptor = new Decryptor({ key })
 describe('Test Default Base64 encryption', () => {
   it('Should return a base64 string', () => {
-    const encryptedDataWithSeparator = encrypt(text, key)
+    const encryptedDataWithSeparator = encryptor.encrypt(text)
     const { ivString, encryptedDataString } = getIvAndEncryptedDataOnly(encryptedDataWithSeparator, 'base64')
     expect(base64Regex.test(ivString)).toBe(true)
     expect(base64Regex.test(encryptedDataString)).toBe(true)
   })
 
   it('Should have a same value with the raw string', () => {
-    const encrypted = encrypt(text, key)
-    const decrypted = decrypt(encrypted, key)
+    const encrypted = encryptor.encrypt(text)
+    const decrypted = decryptor.decrypt(encrypted)
     expect(decrypted).toBe(text)
   })
 })
 
 describe('Test encryption options', () => {
   it('Should return a base64 string', () => {
-    const encryptedData = encrypt(text, key, { encoding: 'base64' })
+    const encryptedData = encryptor.encrypt(text, { encoding: 'base64' })
     const { ivString, encryptedDataString } = getIvAndEncryptedDataOnly(encryptedData, 'base64')
     expect(base64Regex.test(ivString)).toBe(true)
     expect(base64Regex.test(encryptedDataString)).toBe(true)
   })
 
   it('Should have a same value with the raw string', () => {
-    const encrypted = encrypt(text, key, { encoding: 'base64' })
-    const decrypted = decrypt(encrypted, key, { encoding: 'base64' })
+    const encrypted = encryptor.encrypt(text, { encoding: 'base64' })
+    const decrypted = decryptor.decrypt(encrypted, { encoding: 'base64' })
     expect(decrypted).toBe(text)
   })
 
   it('Should can be decrypted', () => {
-    const encrypted = encrypt(text, key, { encryptionCount })
-    const decrypted = decrypt(encrypted, key, { encryptionCount })
+    const encrypted = encryptor.encrypt(text, { encryptionCount })
+    const decrypted = decryptor.decrypt(encrypted, { encryptionCount })
     expect(decrypted).toBe(text)
   })
 
   it('Should work with empty options', () => {
-    const encrypted = encrypt(text, key, {})
-    const decrypted = decrypt(encrypted, key, {})
+    const encrypted = encryptor.encrypt(text, {})
+    const decrypted = decryptor.decrypt(encrypted, {})
     expect(decrypted).toBe(text)
   })
 })
@@ -50,7 +55,7 @@ describe('Test encryption options', () => {
 describe('Test Base64 encryption validation', () => {
   it('Should throw malformated error', () => {
     try {
-      decrypt('as2#', key, { encoding: 'base64' })
+      decryptor.decrypt('as2#', { encoding: 'base64' })
     } catch (error) {
       expect(error instanceof ErrorProvider).toBe(true)
       if (error instanceof ErrorProvider) {
@@ -61,7 +66,7 @@ describe('Test Base64 encryption validation', () => {
 
   it('Should throw invalid key error', () => {
     try {
-      decrypt('asas', 'as2', { encoding: 'base64' })
+      decryptor.decrypt('asas', { encoding: 'base64', key: 'as2' })
     } catch (error) {
       expect(error instanceof ErrorProvider).toBe(true)
       if (error instanceof ErrorProvider) {
@@ -70,7 +75,7 @@ describe('Test Base64 encryption validation', () => {
     }
 
     try {
-      decrypt('asas', '', { encoding: 'base64' })
+      decryptor.decrypt('asas', { encoding: 'base64', key: '' })
     } catch (error) {
       expect(error instanceof ErrorProvider).toBe(true)
       if (error instanceof ErrorProvider) {
@@ -79,7 +84,7 @@ describe('Test Base64 encryption validation', () => {
     }
 
     try {
-      encrypt('asas', 'as2', { encoding: 'base64' })
+      encryptor.encrypt('asas', { encoding: 'base64', key: 'asa' })
     } catch (error) {
       expect(error instanceof ErrorProvider).toBe(true)
       if (error instanceof ErrorProvider) {
@@ -88,7 +93,7 @@ describe('Test Base64 encryption validation', () => {
     }
 
     try {
-      encrypt('asas', '', { encoding: 'base64' })
+      encryptor.encrypt('asas', { encoding: 'base64', key: '' })
     } catch (error) {
       expect(error instanceof ErrorProvider).toBe(true)
       if (error instanceof ErrorProvider) {
@@ -98,10 +103,39 @@ describe('Test Base64 encryption validation', () => {
   })
 
   it('Should not be the same with the raw data', () => {
-    const encrypted = encrypt(text, key, { encryptionCount })
-    const decrypted1 = decrypt(encrypted, key, { encryptionCount: 10 })
-    const decrypted2 = decrypt(encrypted, key, { encryptionCount: 10 })
+    const encrypted = encryptor.encrypt(text, { encryptionCount })
+    const decrypted1 = decryptor.decrypt(encrypted, { encryptionCount: 10 })
+    const decrypted2 = decryptor.decrypt(encrypted, { encryptionCount: 10 })
     expect(decrypted1).not.toBe(text)
     expect(decrypted2).not.toBe(text)
+  })
+})
+
+describe('Test File Encryption', () => {
+  const testPath = join(process.cwd(), '__test__')
+  const sampleImagePath = join(testPath, 'sample.jpg')
+  const encryptedPath = join(testPath, 'sample.sc')
+  const decryptedPath = join(testPath, 'sample-decrypted.jpg')
+  const encryptedFile = encryptor.encryptFile(sampleImagePath, { writeToFile: encryptedPath })
+  it('Should can encrypt a file', () => {
+    expect(base64Regex.test(encryptedFile)).toBe(true)
+  })
+
+  it('Should can decrypt a file', () => {
+    const decrypted = decryptor.decryptFile(encryptedPath, {
+      toBuffer: true,
+      writeToFile: decryptedPath
+    })
+    expect(Buffer.isBuffer(decrypted)).toBe(true)
+  })
+
+  it('Should have the same value with data before encrypted', () => {
+    const sampleJpg = readFileSync(sampleImagePath)
+    const decryptedSample = readFileSync(decryptedPath)
+    expect(sampleJpg).toStrictEqual(decryptedSample)
+  })
+  afterAll(() => {
+    unlinkSync(encryptedPath)
+    unlinkSync(decryptedPath)
   })
 })
--- __test__/hex.test.ts
+++ __test__/hex.test.ts
@@ -1,43 +1,47 @@
-import { decrypt, encrypt } from '../src'
+// tslint:disable: deprecation
+import { readFileSync, unlinkSync } from 'fs'
+import { join } from 'path'
+import { Decryptor, Encryptor } from '../src'
 import { ErrorProvider } from '../src/provider'
 import { hexRegex } from '../src/regex'
 import { getIvAndEncryptedDataOnly } from '../src/utils'
 const key = 'agsbsfsrstsgsfsrsfsrsfsrsgsnshwe'
 const text = 'Hello Fulan!'
 const encryptionCount = 12
-
+const encryptor = new Encryptor({ key })
+const decryptor = new Decryptor({ key })
 describe('Test Default hex encryption', () => {
   it('Should return a hex string', () => {
-    const encryptedData = encrypt(text, key, { encoding: 'hex' })
+    const encryptedData = encryptor.encrypt(text, { encoding: 'hex' })
     const { ivString, encryptedDataString } = getIvAndEncryptedDataOnly(encryptedData, 'hex')
     expect(hexRegex.test(ivString)).toBe(true)
     expect(hexRegex.test(encryptedDataString)).toBe(true)
   })
 
   it('Should have a same value with the raw string', () => {
-    const encrypted = encrypt(text, key, { encoding: 'hex' })
-    const decrypted = decrypt(encrypted, key, { encoding: 'hex' })
+    const encrypted = encryptor.encrypt(text, { encoding: 'hex' })
+    const decrypted = decryptor.decrypt(encrypted, { encoding: 'hex' })
     expect(decrypted).toBe(text)
   })
 })
 
 describe('Test encryption options', () => {
   it('Should return a hex string', () => {
-    const encryptedData = encrypt(text, key, { encoding: 'hex' })
+    const encryptedData = encryptor.encrypt(text, { encoding: 'hex' })
     const { ivString, encryptedDataString } = getIvAndEncryptedDataOnly(encryptedData, 'hex')
     expect(hexRegex.test(ivString)).toBe(true)
     expect(hexRegex.test(encryptedDataString)).toBe(true)
   })
 
   it('Should have a same value with the raw string', () => {
-    const encrypted = encrypt(text, key, { encoding: 'hex' })
-    const decrypted = decrypt(encrypted, key, { encoding: 'hex' })
+    const encrypted = encryptor.encrypt(text, { encoding: 'hex' })
+    const decrypted = decryptor.decrypt(encrypted, { encoding: 'hex' })
     expect(decrypted).toBe(text)
   })
 
   it('Should can be decrypted', () => {
-    const encrypted = encrypt(text, key, { encryptionCount })
-    const decrypted = decrypt(encrypted, key, { encryptionCount })
+    const encrypted = encryptor.encrypt(text, { encryptionCount })
+    const decrypted = decryptor.decrypt(encrypted, { encryptionCount })
     expect(decrypted).toBe(text)
   })
 })
@@ -45,7 +49,7 @@ describe('Test encryption options', () => {
 describe('Test Hex encryption validation', () => {
   it('Should throw malformated error', () => {
     try {
-      decrypt('as2#', key, { encoding: 'hex' })
+      decryptor.decrypt('as2#', { encoding: 'hex' })
     } catch (error) {
       expect(error instanceof ErrorProvider).toBe(true)
       if (error instanceof ErrorProvider) {
@@ -56,7 +60,7 @@ describe('Test Hex encryption validation', () => {
 
   it('Should throw invalid key error', () => {
     try {
-      decrypt('asas', 'as2', { encoding: 'hex' })
+      decryptor.decrypt('asas', { encoding: 'hex', key: 'as2' })
     } catch (error) {
       expect(error instanceof ErrorProvider).toBe(true)
       if (error instanceof ErrorProvider) {
@@ -65,7 +69,7 @@ describe('Test Hex encryption validation', () => {
     }
 
     try {
-      decrypt('asas', '', { encoding: 'hex' })
+      decryptor.decrypt('asas', { encoding: 'hex', key: '' })
     } catch (error) {
       expect(error instanceof ErrorProvider).toBe(true)
       if (error instanceof ErrorProvider) {
@@ -74,7 +78,7 @@ describe('Test Hex encryption validation', () => {
     }
 
     try {
-      encrypt('asas', 'as2', { encoding: 'hex' })
+      encryptor.encrypt('asas', { encoding: 'hex', key: 'as2' })
     } catch (error) {
       expect(error instanceof ErrorProvider).toBe(true)
       if (error instanceof ErrorProvider) {
@@ -83,7 +87,7 @@ describe('Test Hex encryption validation', () => {
     }
 
     try {
-      encrypt('asas', '', { encoding: 'hex' })
+      encryptor.encrypt('asas', { encoding: 'hex', key: '' })
     } catch (error) {
       expect(error instanceof ErrorProvider).toBe(true)
       if (error instanceof ErrorProvider) {
@@ -93,10 +97,46 @@ describe('Test Hex encryption validation', () => {
   })
 
   it('Should not be the same with the raw data', () => {
-    const encrypted = encrypt(text, key, { encryptionCount })
-    const decrypted1 = decrypt(encrypted, key, { encryptionCount: 10 })
-    const decrypted2 = decrypt(encrypted, key, { encryptionCount: 10 })
+    const encrypted = encryptor.encrypt(text, { encryptionCount })
+    const decrypted1 = decryptor.decrypt(encrypted, { encryptionCount: 10 })
+    const decrypted2 = decryptor.decrypt(encrypted, { encryptionCount: 10 })
     expect(decrypted1).not.toBe(text)
     expect(decrypted2).not.toBe(text)
+  })
+})
+
+describe('Test File Encryption', () => {
+  const testPath = join(process.cwd(), '__test__')
+  const sampleImagePath = join(testPath, 'sample.jpg')
+  const encryptedPath = join(testPath, 'sample.sc')
+  const decryptedPath = join(testPath, 'sample-decrypted.jpg')
+  const encryptedFile = encryptor.encryptFile(sampleImagePath, {
+    encoding: 'hex',
+    encryptionCount: 2,
+    writeToFile: encryptedPath
+  })
+
+  it('Should can encrypt a file', () => {
+    expect(hexRegex.test(encryptedFile)).toBe(true)
+  })
+
+  it('Should can decrypt a file', () => {
+    const decrypted = decryptor.decryptFile(encryptedPath, {
+      encoding: 'hex',
+      encryptionCount: 2,
+      toBuffer: true,
+      writeToFile: decryptedPath
+    })
+    expect(Buffer.isBuffer(decrypted)).toBe(true)
+  })
+
+  it('Should have the same value with data before encrypted', () => {
+    const sampleJpg = readFileSync(sampleImagePath)
+    const decryptedSample = readFileSync(decryptedPath)
+    expect(sampleJpg).toStrictEqual(decryptedSample)
+  })
+  afterAll(() => {
+    unlinkSync(encryptedPath)
+    unlinkSync(decryptedPath)
   })
 })
